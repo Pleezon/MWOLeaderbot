31bf2ba4b54f80ddc5e820824a93517eee957969
B4CKF1SH <robin@molexa.de>
2021-01-25 20:11:13 +0100

    Create AddMatch.java

---
+++ src/commands/AddMatch.java
@@ -0,0 +1,11 @@
+package commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class AddMatch extends Command {
+
+    @Override
+    public void exec(MessageReceivedEvent event) {
+
+    }
+}
