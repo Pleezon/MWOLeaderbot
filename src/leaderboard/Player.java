f366fcbc3f7b6d38ef856792cf68d3b4e0153d7d
B4CKF1SH <robin@molexa.de>
2021-01-25 17:03:59 +0100

    Update Player.java

--- src/leaderboard/Player.java
+++ src/leaderboard/Player.java
@@ -1,4 +1,15 @@
 package leaderboard;
 
 public class Player {
+    private double kdr;
+    private double wlr;
+    private int gamesPlayed;
+    private int elo;
+
+    private Player(double kdr, double wlr, int gamesPlayed, int elo) {
+        this.kdr = kdr;
+        this.wlr = wlr;
+        this.gamesPlayed = gamesPlayed;
+        this.elo = elo;
+    }
 }
