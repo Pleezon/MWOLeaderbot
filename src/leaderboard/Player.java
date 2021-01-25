6818ff6c8f0b8713c9d0b678e3f5473fd403953a
B4CKF1SH <robin@molexa.de>
2021-01-25 17:15:42 +0100

    Update Player.java

--- src/leaderboard/Player.java
+++ src/leaderboard/Player.java
@@ -1,15 +1,23 @@
 package leaderboard;
 
+import java.util.UUID;
+
 public class Player {
+    private UUID uniqueId;
     private double kdr;
     private double wlr;
     private int gamesPlayed;
     private int elo;
 
-    private Player(double kdr, double wlr, int gamesPlayed, int elo) {
+    private Player(UUID uniqueId, double kdr, double wlr, int gamesPlayed, int elo) {
+        this.uniqueId = uniqueId;
         this.kdr = kdr;
         this.wlr = wlr;
         this.gamesPlayed = gamesPlayed;
         this.elo = elo;
+    }
+
+    public static Player loadFromDB(UUID uuid) {
+        return null;
     }
 }
